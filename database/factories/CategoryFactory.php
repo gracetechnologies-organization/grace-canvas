c6866ba4b3a0fa243ead51eb4084923969d24ff3
DevWaseem201 <waseem@gracetechnologiespk.com>
2023-08-11 00:41:39 -0700

    seeder category description add

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -18,6 +18,7 @@ class CategoryFactory extends Factory
     {
         return [
             'name' => $this->faker->unique()->word(),
+            'description' => $this->faker->unique()->word(),
             'image' => $this->faker->unique()->word() . '.jpg'
         ];
     }
