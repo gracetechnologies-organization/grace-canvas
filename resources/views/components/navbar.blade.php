26c4f226b4003495506402431c1c0100ce4f1d09
DevWaseem201 <waseem@gracetechnologiespk.com>
2023-08-21 05:48:00 -0700

    icons changes

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,32 +1,3 @@
-<!-- <nav class="navbar navbar-expand-lg navbar-light " id="navbarManu">
-    <div class="container">
-        <a class="navbar-brand text-light" href="#">{{ config('app.name') }}</a>
-        <button class="navbar-toggler text-light" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbar-supported-content" aria-controls="navbar-supported-content" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon text-light"></span>
-        </button>
-        <div class="collapse navbar-collapse text-light" id="navbar-supported-content">
-            <ul class="navbar-nav ms-auto ">
-                <li class="nav-item ">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Templates</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">More Services</a>
-                </li>
-                <li class="nav-item sign-up">
-                    <a class="nav-link" href="#">Sign up</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav> -->
 <nav class="navbar navbar-expand-lg navbar-light " id="navbarManu">
     <div class="container ">
         <a class="navbar-brand text-light" href="#">Logo hare</a>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <link rel="icon" type="image/x-icon" href="{{ asset('storage/icons/canvas_icon.webp') }}" />
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
 
     <!-- Core theme CSS (includes Bootstrap v5.2.3)-->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet" />
