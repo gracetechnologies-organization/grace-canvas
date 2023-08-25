7c92ea78e2d9013d000f9c180efbacbc6c51f445
gracetechnologies <appfitkare@gmail.com>
2023-08-25 19:40:39 +0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
         {{-- @livewire('navigation-menu') --}}
-
+{{--  --}}
         <!-- Page Heading -->
         @if (isset($header))
             <header class="bg-white shadow dark:bg-gray-800">
