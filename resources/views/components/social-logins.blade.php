e28143dac8e5fa002fe599885440f3449456b30d
gracetechnologies <appfitkare@gmail.com>
2023-10-10 10:56:15 +0500

    Translation included for social logins

--- resources/views/components/social-logins.blade.php
+++ resources/views/components/social-logins.blade.php
@@ -2,14 +2,14 @@
     <div class="mb-2">
         <a href="{{ route('google.page') }}">
             <button type="submit" class="p-2 custom-google-login-signup btn-block">
-                <span class="bi bi-google"></span>{{ GoogleTranslate::trans('Google Login', app()->getLocale()) }}
+                <span class="bi bi-google"></span>{{ app('googleTranslator', ['string' => 'Google Login']) }}
             </button>
         </a>
     </div>
      <div>
         <a href="{{ route('linkedin.page') }}">
             <button type="submit" class="p-2 custom-linkedin-login-signup btn-block">
-                <span class="bx bxl-linkedin bx-linkedin"></span>{{ GoogleTranslate::trans('LinkedIn Login', app()->getLocale()) }}
+                <span class="bx bxl-linkedin bx-linkedin"></span>{{ app('googleTranslator', ['string' => 'LinkedIn Login']) }}
             </button>
         </a>
     </div>
