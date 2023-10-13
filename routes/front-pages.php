ed286ccea307eb894e80f2146078cc91380e7fb2
gracetechnologies <appfitkare@gmail.com>
2023-10-13 15:38:14 +0500

    Sitemap Del + All Bots Disallowed

--- routes/front-pages.php
+++ routes/front-pages.php
@@ -3,7 +3,11 @@
 use App\Http\Livewire\FrontPages\HomePage;
 use App\Http\Livewire\FrontPages\ResumeTemplates;
 
-Route::get('/', HomePage::class)->name('home');
+// Route::get('/', HomePage::class)->name('home');
+
+Route::get('/', function () {
+    return "Grace Canvas has been moved to onlineresumaker";
+});
 
 Route::prefix('templates')->group(function () {
     Route::get('/resume', ResumeTemplates::class)->name('templates.resume');
