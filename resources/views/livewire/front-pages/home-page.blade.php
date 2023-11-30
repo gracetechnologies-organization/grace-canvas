75a9593470cf3defcdfc89480e7b06babe1afd3b
gracetechnologies <appfitkare@gmail.com>
2023-11-30 17:15:07 +0500

    Extra Code Removed

--- resources/views/livewire/front-pages/home-page.blade.php
+++ resources/views/livewire/front-pages/home-page.blade.php
@@ -1,3 +1,3 @@
-<div>
-    <p>Grace Canvas has been moved to onlineresumaker</p>
+<div class="p-3">
+    <p>Grace Canvas has been moved to onlineresumaker & this project now only serve API's</p>
 </div>
--- routes/front-pages.php
+++ routes/front-pages.php
@@ -1,10 +1,5 @@
 <?php
 
 use App\Http\Livewire\FrontPages\HomePage;
-use App\Http\Livewire\FrontPages\ResumeTemplates;
 
 Route::get('/', HomePage::class)->name('home');
-
-// Route::prefix('templates')->group(function () {
-//     Route::get('/resume', ResumeTemplates::class)->name('templates.resume');
-// });
